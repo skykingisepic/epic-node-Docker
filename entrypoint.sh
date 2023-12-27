a5667e8afede09e4a9b871ad75befcee65d0fcba
Sky King <skyking@epic.tech>
2023-12-27 08:17:46 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,9 @@ if [ -e ~/.epic/main/foundation.json ]
 then
 	echo "Skipping"
 else
-	echo "Setting up bootstrap chain_data file..."
+	echo "Finishing Up..."
 	mkdir -p ~/.epic
 	mkdir -p ~/.epic/main
-	wget https://epiccash.s3.sa-east-1.amazonaws.com/mainnet.zip -P ~/.epic/main
-	unzip ~/.epic/main/mainnet.zip -d ~/.epic/main
-	rm ~/.epic/main/mainnet.zip
 	cp /epic/* ~/.epic/main
 	echo "Done"
 fi
